ddde4ba621a79215fcff6ecc0fcbee494d350f41
Keenan Thompson <keenan@keenant.com>
2018-10-28 19:31:14 -0500

    GroupMemberships model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,10 @@
 class Group < ApplicationRecord
   belongs_to :category
   has_many :events
+  has_many :group_memberships
+  has_many :users, through: :group_memberships
+
+  validates :name, length: { in: 3..30 }
+  validates :description, length: { maximum: 1000 }
+  validates :category, presence: true 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181022161743) do
+ActiveRecord::Schema.define(version: 20181029002651) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20181022161743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_events_on_group_id"
+  end
+
+  create_table "group_memberships", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_memberships_on_group_id"
+    t.index ["user_id"], name: "index_group_memberships_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
