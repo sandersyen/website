bc333709a59cb40d47015d0b51cf0c4d5503a91c
bmoe24x <38509504+bmoe24x@users.noreply.github.com>
2018-11-11 20:38:34 -0600

    Added join a group functionality + some css cleanup on the login

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :set_group, only: [:show, :edit, :update, :destroy, :join_group ]
 
   # GET /groups
   def index
@@ -28,12 +28,41 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @membership = @group.group_memberships.new(user: current_user)
 
+    # @group.group_memberships.where(user: current_user).destroy
+
     if @group.save && @membership.save
       redirect_to @group, notice: 'Group was successfully created.'
     else
       render :new
     end
   end
+
+
+
+  # POST /groups/:id:/join
+  def join_group
+    return if enforce_login(@group)
+    # *** put in enforcing permissions! ***
+
+    if( @group.users.include?(current_user))
+      redirect_to @group, notice: 'User already in group!'
+      return
+    end
+
+    @membership = @group.group_memberships.new(user: current_user)
+
+    if @membership.save
+      redirect_to @group, notice: 'Joined group successfully!'
+    else
+      render :new
+    end
+  end
+
+
+  # @group.group_memberships.where(user: current_user).destroy
+
+
+
 
   # PATCH/PUT /groups/1
   def update
