a370e1178853c1a82aa72db328ad5a77751afd69
Keenan Thompson <keenan@keenant.com>
2018-11-14 00:13:42 -0600

    Add role field to memberships

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -1,4 +1,18 @@
 class GroupMembership < ApplicationRecord
+  ROLES = ['USER', 'ADMIN']
+
   belongs_to :group
   belongs_to :user
+
+  validates :group, presence: true
+  validates :user, presence: true
+  validates :role, inclusion: {in: ROLES}
+
+  def is_user?
+    self.role == 'USER'
+  end
+
+  def is_admin?
+    self.role == 'ADMIN'
+  end
 end
