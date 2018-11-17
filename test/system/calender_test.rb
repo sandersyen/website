9832b9759449f2fdfef8787254a50388050b81be
Keenan Thompson <keenan@keenant.com>
2018-11-16 19:13:29 -0600

    Fix calendar test

--- test/system/calender_test.rb
+++ test/system/calender_test.rb
@@ -2,8 +2,11 @@ require "application_system_test_case"
 
 class CalenderTests < ApplicationSystemTestCase
     test "calender" do
-        visit simulate_login_path
+        # performs login, creates test group and test event
         event = create_test_event
+        # make sure our test event is in the current calendar
+        event.update(start_time: Time.now + 1.hour, end_time: Time.now + 2.hours)
+        
         visit home_path
         assert page.has_content?('Event1')
     end
