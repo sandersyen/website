e9d486126c00f592613328322f12a1031d0d7b1d
bmoe24x <38509504+bmoe24x@users.noreply.github.com>
2018-11-28 21:13:30 -0600

    makes the search only return visible groups and events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,11 +16,11 @@ class PagesController < ApplicationController
    # @groups = Group.all
    @groups = Group.where( is_viewable: true )
     if (params[:search_event])
-      @events = Event.where("name LIKE :search_event OR description LIKE :search_event OR location LIKE :search_event", search_event: "%#{params[:search_event]}%")
+      @events = Event.where("name LIKE :search_event OR description LIKE :search_event OR location LIKE :search_event", search_event: "%#{params[:search_event]}%").where( is_viewable: true )
     end
 
     if (params[:search_group])
-      @groups = Group.where("name LIKE :search_group OR description LIKE :search_group", search_group: "%#{params[:search_group]}%")
+      @groups = Group.where("name LIKE :search_group OR description LIKE :search_group", search_group: "%#{params[:search_group]}%").where( is_viewable: true )
     end
   end
 end
